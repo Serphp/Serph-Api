710fd4a755d72ce40ecd1d106d02ece7d6ea6df5
Bryan Rodriguez <Serphp@hotmail.com>
2022-11-06 15:33:47 -0600

    Update Navbar whit NavLink

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,12 +1,12 @@
-import { Link } from "react-router-dom";
+import { Link , NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div className="navbar navbar-dak bg-dark">
             <div className="container">
-            <Link className="btn btn-outline-primary" to="/"> Inicio </Link>
-            <Link className="btn btn-outline-primary" to="/blog"> blog </Link>
-            <Link className="btn btn-outline-primary" to="/Contacto"> Contacto </Link>
+            <NavLink className="btn btn-outline-primary" to="/"> Inicio </NavLink>
+            <NavLink className="btn btn-outline-primary" to="/blog"> blog </NavLink>
+            <NavLink className="btn btn-outline-primary" to="/Contacto"> Contacto </NavLink>
             </div>
         </div>
     )
